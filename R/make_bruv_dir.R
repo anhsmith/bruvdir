ce8c01ec8b09bd5d5026412c805287bfbacc0b63
Adam <anhsmith@gmail.com>
2024-03-14 09:35:26 +1300

    fix problem with 09 to 10

--- R/make_bruv_dir.R
+++ R/make_bruv_dir.R
@@ -12,7 +12,7 @@ make_bruv_dir <- function(
   for( i in 1:no_sets ) {
 
     # choose set name S01
-    set_name <- paste0("S0", i)
+    set_name <- if(i < 10) paste0("S0", i) else paste0("S", i)
 
     # create set folder BRUV/BRUV_S01
     dir.create(
